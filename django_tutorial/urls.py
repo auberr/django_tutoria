448f301bc8e0ba9b4afaed37cb7983aa4b5e134a
woong-garden <dndwn7007@gmail.com>
2022-09-23 22:44:53 +0900

    메뉴선정 페이지~

--- django_tutorial/urls.py
+++ django_tutorial/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from articles import views #articles에서 views파일 가져오기
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('dinner/<str:name>/',views.dinner),
 ]
